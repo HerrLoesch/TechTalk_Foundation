8020594ea84d0a164fdd3121a1bb480e3b97d2f4
Hendrik Lösch <loeschhendrik@gmail.com>
2022-11-25 13:44:06 +0100

    Added BrewingService

---
+++ CoffeeBrewing.Contracts/IBrewingService.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IBrewingService.cs" company="Carl Zeiss">
+//   © ZEISS. All rights reserved.
+// </copyright>
+
+using UnitsNet;
+using Zeiss.Semi.Mask.Foundation.Common.Contracts.ErrorHandling;
+
+namespace CoffeeBrewing.Contracts;
+
+public interface IBrewingService
+{
+    Task<Result> Brew(Mass coffee, Volume water);
+}
---
+++ CoffeeBrewing.Implementation/BrewingService.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="BrewingService.cs" company="Carl Zeiss">
+//   © ZEISS. All rights reserved.
+// </copyright>
+
+using CoffeeBrewing.Contracts;
+using CoffeeBrewing.Implementation.BrewingSteps;
+using UnitsNet;
+using Zeiss.Semi.Mask.Foundation.Common.Contracts.ErrorHandling;
+using Zeiss.Semi.Mask.Foundation.SequenceEngine.Implementation;
+
+namespace CoffeeBrewing.Implementation;
+
+public class BrewingService : IBrewingService
+{
+    private readonly BrewingSequenceBuilder sequenceBuilder;
+
+    public BrewingService(BrewingSequenceBuilder sequenceBuilder)
+    {
+        this.sequenceBuilder = sequenceBuilder;
+    }
+
+    public Task<Result> Brew(Mass coffee, Volume water)
+    {
+        var sequence = new Sequence<CoffeeRecipe>(this.sequenceBuilder);
+        return sequence.RunAsync(new CoffeeRecipe(coffee, water));
+    }
+}
